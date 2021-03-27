3360caf3364245b2533477ae69f005150cb4ed90
HOYIN CHAN <hychan1990@gmail.com>
2021-03-27 18:30:24 +0800

    init

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+// Copyright 2018 The Flutter team. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Welcome to Flutter',
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: new Text('Welcome to Flutter title in appBar'),
+        ),
+        body: new Center(
+          child: new Text('Hello World222'),
+        ),
+      ),
+    );
+  }
+}
